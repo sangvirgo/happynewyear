9fe94562152ff64fa274cf7b2d49dc15894e4f49
Sang Virgo <tansang06092004@gmail.com>
2024-02-09 23:18:25 +0700

    hih

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy new year';
+const titleCard = 'Chúc mừng năm mới vợ của anh';
+const contentCard = 'Bằng tận đáy lòng của mình anh chúc em năm mới tràn đầy niềm vui bên anh và gia đình, chúc em luôn đặt được những gì mình muốn, chúc em luôn tươi cười và luôn là người yêu bé bỏng của anh nhé. Năm sau tụi mình càng iu nhau nhiều hơn nữa nhé iu em lắm luôn. Và cảm ơn em vì năm qua đã đến bên cạnh anh luôn bên anh. ANH IU EM lắm luôn.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
